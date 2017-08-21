fcb3525c98fb37b402fe4223e14c46e2bafbf454
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2017-08-21 18:11:50 -0400

    changed logic and ux

--- js/setup.js
+++ js/setup.js
@@ -1,56 +1,72 @@
-var canvas  = document.querySelector('canvas');
+var canvas, canvasContext;
 
-canvas.width = window.innerWidth = 800;
-canvas.height = window.innerHeight = 600;
+function updateMousePos(e) {
+  var rect = canvas.getBoundingClientRect();
+  var root = document.documentElement;
+  var mouseX = e.clientX - rect.left - root.scrollLeft;
+  // var mouseY = e.clientY - rect.top = root.scrollTop;
 
-var c = canvas.getContext('2d');
-
-// c.fillStyle = 'rgba(255, 0, 0, 0.1 )'; // the fill style has to go before the "fill Rect()"
-// c.fillRect(100, 100, 100, 100);
-//
-//
-//
-// // moving line
-// c.beginPath();
-// c.moveTo(50, 300);
-// c.lineTo(300, 100);
-// c.strokeStyle = "#aaa"
-// c.stroke();
-
-// arc / Circle
-// c.beginPath();
-// c.arc(300, 300, 30, 0, Math.PI * 2, false);
-// c.strokeStyle = 'blue'
-// c.stroke();
-
-// arc / Circle
-
-var x = 300;
-var y = 300;
-var dx = 4;
-var dy = 4;
-var radius = 30;
-
-function animate() {
-  requestAnimationFrame(animate);
-  c.clearRect(0, 0, innerWidth, innerHeight);
-
-  c.beginPath();
-  c.arc(x, y, 30, 0, Math.PI * 2, false);
-  c.strokeStyle = 'blue'
-  c.stroke();
-
-  if (x + radius > innerWidth || x - radius < 0) {
-    dx = -dx;
-  }
-
-  if (y + radius > innerHeight || y - radius < 0) {
-    dy = -dy;
-  }
-
-  x += dx;
-  y += dy;
-  console.log('procesing ...');
+  paddleX = mouseX - PADDLE_WIDTH/2;
 }
 
-animate();
+window.onload = function() {
+
+  canvas = document.getElementById('gameCanvas');
+  ctx = canvas.getContext('2d');
+
+  var framesPerSecond = 30;
+  setInterval(updateAll, 1000/framesPerSecond);
+
+  canvas.addEventListener('mousemove', updateMousePos);
+}
+
+var ballX = 75;
+var ballY = 75;
+var radius = 10;
+
+var ballSpeedX = 5;
+var ballSpeedY = 5;
+
+const PADDLE_WIDTH = 100;
+const PADDLE_HEIGHT = 10;
+var paddleX = 400;
+
+function updateAll() {
+  moveAll();
+  drawAll()
+}
+
+// this controls the boundraries of the ball movement but doesn't create the movement itself;
+function moveAll() {
+  ballX += ballSpeedX;
+  ballY += ballSpeedY;
+
+  if (ballX + radius > canvas.width || ballX - radius < 0) {
+    ballSpeedX = -ballSpeedX;
+  }
+
+  if (ballY + radius > canvas.height   || ballY - radius < 0) {
+    ballSpeedY = -ballSpeedY;
+  }
+}
+
+//drawing the canvas and the ball everytime the window loads.
+// it emulates movement
+function drawAll() {
+  colorRect(0, 0, canvas.width, canvas.height, 'black') //draw screen
+  colorCircle(ballX, ballY, radius, 'white' )           //draw ball
+
+  colorRect(paddleX, canvas.height - PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_HEIGHT, 'white');
+}
+
+function colorRect(topLeftX, topLeftY, boxWidth, boxHeight, fillColor){
+  ctx.fillStyle = fillColor;
+  ctx.fillRect(topLeftX, topLeftY, boxWidth, boxHeight);
+}
+
+function colorCircle(centerX, centerY,  ballRadius, fillColor) {
+  ctx.fillStyle = fillColor;
+  ctx.beginPath();
+  ctx.arc(centerX, centerY,  ballRadius, 0, Math.PI * 2, true);
+  ctx.fill();
+}
